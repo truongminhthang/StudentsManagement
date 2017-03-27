f99c8814dc1aae1201d2d2e2de53ffffd03150fc
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-27 18:51:31 +0700

    Create Student Model and Test Student Model

---
+++ StudentsManagement/Student.swift
@@ -0,0 +1,24 @@
+//
+//  Student.swift
+//  StudentsManagement
+//
+//  Created by Trương Thắng on 3/27/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import Foundation
+
+class Student: NSObject {
+    
+    var name: String
+    var phoneNumber: String
+    
+    init?(name: String, phoneNumber: String) {
+        guard name != "" else {return nil}
+        guard phoneNumber.characters.count > 10 else {return nil}
+        self.name = name
+        self.phoneNumber = phoneNumber
+    }
+    
+    
+}
---
+++ StudentsManagementTests/StudentsManagementTests.swift
@@ -0,0 +1,34 @@
+//
+//  StudentsManagementTests.swift
+//  StudentsManagementTests
+//
+//  Created by Trương Thắng on 3/27/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import XCTest
+
+class StudentTest: XCTestCase {
+    
+   
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testStudent() {
+        XCTAssertTrue(Student(name: "", phoneNumber: "01239180123") == nil)
+        XCTAssertTrue(Student(name: "Thang", phoneNumber: "0123") == nil)
+        XCTAssertTrue(Student(name: "", phoneNumber: "") == nil)
+        XCTAssertTrue(Student(name: "Thang", phoneNumber: "01239180123") != nil)
+
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
